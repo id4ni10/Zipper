35742ae7a5e91f7c2fd553d1587a832cdd91bf12
Danilo <id4ni10@gmail.com>
2016-02-26 16:46:27 -0300

    First commit!

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System.IO;
+
+public class Program
+{
+    static int Main(string[] args)
+    {
+        var zip = ZipFiles(_pack);
+
+        File.WriteAllBytes(@"..\file.rar", zip);
+
+        return 0;
+    }
+
+    public byte[] ZipFiles(Dictionary<string, byte[]> arquivos)
+    {
+        try
+        {
+            using (var zip = new ZipFile())
+            {
+                foreach (var filename in arquivos.Keys)
+                {
+                    zip.AddEntry(filename, arquivos[filename]);
+                }
+
+                zip.Comment = String.Format("Este arquivo foi gerado por: 'SISGED Capture'\n'{0}'",
+                   System.Net.Dns.GetHostName());
+
+                var ms = new MemoryStream();
+                zip.Save(ms);
+
+                return ms.ToArray();
+            }
+        }
+        catch (System.Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
